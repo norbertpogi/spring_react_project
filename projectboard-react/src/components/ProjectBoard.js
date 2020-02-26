b9659cf1dc3e76009561460bb84ce0051d683cfb
norbertpogi <norbert.bautista.99@gmail.com>
2020-02-26 13:54:21 +0800

    display no project task on board if no record found in DB

--- projectboard-react/src/components/ProjectBoard.js
+++ projectboard-react/src/components/ProjectBoard.js
@@ -2,16 +2,86 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import ProjectTaskItem from './ProjectTask/ProjectTaskItem';
 import PropTypes from 'prop-types';
-import {getBacklog} from '../actions/projectTaskActions';
+import { getBacklog } from '../actions/projectTaskActions';
 import { connect } from 'react-redux';
 
 class ProjectBoard extends Component {
 
     componentDidMount() {
-        this.props.getBacklog();        
+        this.props.getBacklog();
     }
 
     render() {
+        const { project_tasks } = this.props.project_tasks;
+
+        let BoardContent;
+        let todoItems = []
+        let inProgressItems = []
+        let doneItems = []
+
+        const BoardAlgorithm = project_tasks => {
+            if (project_tasks.length < 1) {
+                console.log('0000000000000000000000000 ' + project_tasks.length);
+                return (
+                    <div className="alert alert-info text-center" role="alert">
+                        No Project Task on this board
+                    </div>
+                );
+            } else {
+                const tasks = project_tasks.map(project_task => (
+                    <ProjectTaskItem key={project_task.id} project_tasks={project_task} />
+                ));
+
+                for (let i = 0; i < tasks.length; i++) {
+                    
+                    if (tasks[i].props.project_tasks.status === "TO_DO") {
+                        todoItems.push(tasks[i]);
+                    }
+                    if (tasks[i].props.project_tasks.status === "IN_PROGRESS") {
+                        inProgressItems.push(tasks[i]);
+                    }
+                    if (tasks[i].props.project_tasks.status === "DONE") {
+                        doneItems.push(tasks[i]);
+                    }
+                }
+                return (
+                    <React.Fragment>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-secondary text-white">
+                                            <h3>TO DO</h3>
+                                        </div>
+                                    </div>
+                                    {todoItems}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-primary text-white">
+                                            <h3>In Progress</h3>
+                                        </div>
+                                    </div>
+                                    {inProgressItems}
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="card text-center mb-2">
+                                        <div className="card-header bg-success text-white">
+                                            <h3>Done</h3>
+                                        </div>
+                                    </div>
+                                    {doneItems}
+                                </div>
+                            </div>
+                        </div>
+                    </React.Fragment>
+                );
+
+            }
+        };
+
+        BoardContent = BoardAlgorithm(project_tasks);
+
         return (
             <div className="container">
                 <Link to="/AddProjectTask" className="btn btn-primary mb-3">
@@ -19,34 +89,7 @@ class ProjectBoard extends Component {
                 </Link>
                 <br />
                 <hr />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-secondary text-white">
-                                    <h3>TO DO</h3>
-                                </div>
-                            </div>
-                            <ProjectTaskItem />
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-primary text-white">
-                                    <h3>In Progress</h3>
-                                </div>
-                            </div>
-                            <ProjectTaskItem />
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card text-center mb-2">
-                                <div className="card-header bg-success text-white">
-                                    <h3>Done</h3>
-                                </div>
-                            </div>
-                            <ProjectTaskItem />
-                        </div>
-                    </div>
-                </div>
+                {BoardContent}
             </div>
         );
     }
@@ -58,7 +101,7 @@ ProjectBoard.propTypes = {
 };
 
 const mapStateToProps = state => ({
-    project_tasks: state.project_tasks
-}) 
+    project_tasks: state.project_task
+})
 
-export default connect(mapStateToProps, {getBacklog})(ProjectBoard);
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
