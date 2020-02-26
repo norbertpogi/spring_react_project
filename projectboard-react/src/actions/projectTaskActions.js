9ec3a8b8b2b8b84050b7e5ded3371d9d856867db
norbertpogi <norbert.bautista.99@gmail.com>
2020-02-26 17:15:21 +0800

    update delete function and redux filter

--- projectboard-react/src/actions/projectTaskActions.js
+++ projectboard-react/src/actions/projectTaskActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, GET_PROJECT_TASKS } from './types';
+import { GET_ERRORS, GET_PROJECT_TASKS, DELETE_PROJECT_TASK } from './types';
 
 const api_endpoint = 'http://localhost:8080/api/board';
 
@@ -25,4 +25,14 @@ export const getBacklog = () => async dispatch => {
         type: GET_PROJECT_TASKS,
         payload: res.data
     })
+};
+
+export const deleteProjectTask = pt_id => async dispatch => {
+    if (window.confirm(`You are deleting project task ${pt_id}, this action cannot be undone`)) {        
+        await axios.delete(api_endpoint + '/' + pt_id );
+        dispatch({
+            type: DELETE_PROJECT_TASK,
+            payload: pt_id
+        });
+    }
 }
--- projectboard-react/src/components/ProjectTask/ProjectTaskItem.js
+++ projectboard-react/src/components/ProjectTask/ProjectTaskItem.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react'
+//import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteProjectTask } from '../../actions/projectTaskActions';
 
 class ProjectTaskItem extends Component {
+
+    onDeleteClick(pt_id){
+        console.log('id id is : ' + pt_id);
+        this.props.deleteProjectTask(pt_id);
+    }
+
     render() {
+        const { project_tasks } = this.props;
         return (
             <div className="card mb-1 bg-light">
                 <div className="card-header text-primary">
-                    ID: projectSequence
+                    ID: {project_tasks.id}
                 </div>
                 <div className="card-body bg-light">
-                    <h5 className="card-title">summary</h5>
-                    <p className="card-text text-truncate ">acceptanceCriteria</p>
+                    <h5 className="card-title">{project_tasks.summary}</h5>
+                    <p className="card-text text-truncate ">{project_tasks.acceptanceCriteria}</p>
                     <a href='' className="btn btn-primary"> View / Update</a>
-                    <button className="btn btn-danger ml-4">Delete</button>
+                    <button className="btn btn-danger ml-4"
+                    onClick={this.onDeleteClick.bind(this, project_tasks.id)}
+                    >Delete</button>
                 </div>
             </div>
         )
     }
 }
+ProjectTaskItem.prototypes = {
+    deleteProjectTask: PropTypes.func.isRequired
+   // project_tasks: PropTypes.object.isRequired
+}
 
-export default ProjectTaskItem;
+// const mapStateToProps = state => ({
+//     project_tasks: state.project_tasks
+// })
+
+export default connect(null, {deleteProjectTask}) (ProjectTaskItem);
